25fba1b633b6dcbdfdc73f188b526c574a41b38f
taniguchikoei <73098168+taniguchikoei@users.noreply.github.com>
2020-10-19 18:11:37 +0900

    Change section 1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -3,9 +3,9 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  // background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  // fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
