d210053561e7324ae778fbd4fb8dd14059f63574
taniguchikoei <73098168+taniguchikoei@users.noreply.github.com>
2020-10-20 14:10:44 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,12 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+function setup() {
+  for(let i=0; i<10; i++)
+    if(i<5){
+      stroke(255,0,0);
+ellipse(50,50,100-i*10);
+    }
+  else{
+    stroke(0,0,255);
+    ellipse(50,50,100-i*10);
   }
 }
